eb1d0cca1b87e61b162fba33a576210e01bcb14c
Lennart <lennart@madmanfred.com>
2019-08-23 19:41:18 +0200

    Cleaned up thrift-worker.

--- src/thrift-worker/worker.cpp
+++ src/thrift-worker/worker.cpp
@@ -18,35 +18,13 @@ using namespace example;
 
 using namespace std;
 
-void main_cpp(int, char**);
-void open_cpp();
-void close_cpp();
-void ping_cpp();
-void echo_cpp(char* text, int size);
-
 std::shared_ptr<TTransport> __transport;
 std::shared_ptr<ExampleClient> __client;
 
 extern "C"
 {
-int finished = 0;
-
-void callback(char* data, int size){
-	char working[8] = "WORKING";
-	char finishedText[9] = "FINISHED";
-	if(finished == 0){
-		emscripten_worker_respond(working , 8);
-	}else{
-		emscripten_worker_respond(finishedText ,9);
-	}
-}
 
 void tw_open(char* data, int size){
-	//~ TSocket* socket_ = new TSocket("127.0.0.1", 9090);
-	//~ std::shared_ptr<TTransport> socket(socket_);
-	//~ std::shared_ptr<TTransport> transport(new TBufferedTransport(socket));
-	//~ std::shared_ptr<TProtocol> protocol(new TBinaryProtocol(transport));
-	//~ ExampleClient client(protocol);
 	TSocket* socket_ = new TSocket("127.0.0.1", 9090);
 	std::shared_ptr<TTransport> socket(socket_);
 	std::shared_ptr<TTransport> ___transport(new TBufferedTransport(socket));
@@ -55,7 +33,7 @@ void tw_open(char* data, int size){
 	std::shared_ptr<ExampleClient> ___client(new ExampleClient(protocol));
 	__client = ___client;
 	__transport->open();
-	emscripten_worker_respond("e", 2);
+	emscripten_worker_respond(nullptr, 0);
 }
 
 
@@ -80,20 +58,6 @@ void tw_echo(char* data, int size){
 }
 
 int main(int i, char** c){
-	main_cpp(i, c);
 }
-}
-
-void main_cpp(int, char**)
-{
-}
-
-void open_cpp(){
-}
-void close_cpp(){
-}
-void ping_cpp(){
-}
-void echo_cpp(char* text, int size){
 }
 
